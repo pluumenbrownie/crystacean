c1188c270c60b9bca3aefaf9ed4d1ad50d240548
Pluumenbrownie <git.wessel@beumertjes.nl>
2024-03-16 16:43:51 +0100

    Added graph formatting

--- pyo_test.py
+++ pyo_test.py
@@ -1,7 +1,12 @@
 from lattice_solver_python import Lattice, test_module, from_dft_json
 from findthosepoints import full_lattice_from_basis_vectors
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
 from tqdm import tqdm
+
+rcParams.update({'font.size': 16})
+lattice_size = 10
+site_size = 16
 
 test_module()
 
@@ -62,23 +67,28 @@ bigger_boundary_points = [
 # basis_vectors = [[1.5, 0.0, 0.0], [0.75, 1.299038105676658, 0.0], [0.0, 0.0, 1.5]]
 
 
-# custom_boundary_points = full_lattice_from_basis_vectors(size=2)
-# lattice = Lattice(custom_boundary_points, 1.1)
+custom_boundary_points = full_lattice_from_basis_vectors(size=1)
+lattice = Lattice(custom_boundary_points, 1.1)
 
-lattice = from_dft_json("DFT_results/T6l.json", 3.5, False)
+# lattice = from_dft_json("DFT_results/T1S.json", 3.5, False)
 # lattice.diagnostic_ase()
 # for num, point in enumerate(zip( *lattice.points_to_plot())):
 #     plt.annotate(str(num), (point[0], point[1]))
 
-plt.plot( *lattice.points_to_plot(), "o")
-plt.plot( *lattice.midpoints_to_plot(), "x")
-plt.plot( *lattice.tripoints_to_plot(), "s")
-plt.plot( *lattice.singlets_to_plot(), "^")
-# for num, x, y in lattice.no_rings_plot():
-#     plt.annotate(str(num), (x, y))
-plt.show()
+# plt.plot( *lattice.points_to_plot(), "o", markersize=lattice_size)
+# plt.plot( *lattice.midpoints_to_plot(), "x", markersize=site_size)
+# plt.plot( *lattice.tripoints_to_plot(), "s", markersize=site_size)
+# plt.plot( *lattice.singlets_to_plot(), "^", markersize=site_size)
+# plt.axis('equal')
+# plt.xlim(0, 5)
+# plt.ylim(-5, 3)
+# plt.xlabel("x (Å)")
+# plt.ylabel("y (Å)")
+# # for num, x, y in lattice.no_rings_plot():
+# #     plt.annotate(str(num), (x, y))
+# plt.show()
 
-noloops = lattice.no_rings()
+# noloops = lattice.no_rings()
 
 bit_lattice = lattice.get_intermediary()
 print(bit_lattice)
@@ -87,31 +97,40 @@ print(bit_lattice)
 # exit()
 solutions = bit_lattice.solve(True)
 # solutions = filtered_bit.solve(True)
-
+# exit()
 progress = tqdm(
     enumerate(solutions),
     total=len(solutions),
     bar_format="{l_bar}{bar}| {n_fmt}/{total_fmt}  "
 )
 
-# to_show = {0, 1, 2, 3, 4, 8, 9}
+# to_show = {0, 19}
 for number, solution in progress:
     solved_lattice = lattice.to_solved_lattice(solution)
     # solved_lattice.export("exports/T6_noloops", f"T6_{number:>06}.json")
-    solved_lattice.export_as_ase_json(f"exports/T6_loops/T6_{number:>06}.json")
+    # solved_lattice.export_as_ase_json(f"exports/T6_noloops/T6_{number:>06}.json")
 
     # if number in to_show:
-        # solved_lattice.export_as_ase_json(f"exports/sizeable_remade/sizeable_rust_{number:>06}.json")
-    #     progress.set_description(desc=f"Solution {number}")
-    #     # print(f"Solution {number}")
-    #     plt.plot( *solved_lattice.points_to_plot(), "o")
-    #     for num, point in enumerate(zip( *lattice.points_to_plot())):
-    #         plt.annotate(str(num), (point[0], point[1]))
+    # solved_lattice.export_as_ase_json(f"exports/sizeable_remade/sizeable_rust_{number:>06}.json")
+    progress.set_description(desc=f"Solution {number}")
+#     # print(f"Solution {number}")
+    plt.plot( *solved_lattice.points_to_plot(), "o", markersize=lattice_size)
+#     for num, point in enumerate(zip( *lattice.points_to_plot())):
+#         plt.annotate(str(num), (point[0], point[1]))
 
-    #     plt.plot( *solved_lattice.midpoints_to_plot(), "x")
-    #     plt.plot( *solved_lattice.tripoints_to_plot(), "s")
-    #     plt.plot( *solved_lattice.singlets_to_plot(), "^", markersize=10)
-    #     for num, point in enumerate(zip( *solved_lattice.oxygens_to_plot())):
-    #         plt.annotate(str(num), (point[0], point[1]))
-    #     plt.show()
+    plt.plot( *solved_lattice.midpoints_to_plot(), "x", markersize=site_size)
+    plt.plot( *solved_lattice.tripoints_to_plot(), "s", markersize=site_size)
+    plt.plot( *solved_lattice.singlets_to_plot(), "^",  markersize=site_size)
+#     for num, point in enumerate(zip( *solved_lattice.oxygens_to_plot())):
+#         plt.annotate(str(num), (point[0], point[1]))
+    plt.axis('equal')
+    plt.xlim(0, 5)
+    plt.ylim(-5, 3)
+    plt.xlabel("x (Å)")
+    plt.ylabel("y (Å)")
+    # plt.title("")
+    # plt.legend(loc=)
+    # plt.show()
+    
+    plt.show()
 # print(rust_those_points.print_in_rust((1, 2)))
