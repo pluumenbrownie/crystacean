pluumenbrownie/crystacean

commit b68d78f6231ee70da6a12ae05fc4ed3f0e333f3e

We are SO BACK

lattice_solver/examples/solve.rs
@@ -40,5 +40,5 @@ fn main() {
     let lattice = Lattice::python_new(lattice_points, 1.1, true);
     let bit_lattice = lattice.get_intermediary();
 
-    bit_lattice.solve(true);
+    let _ = bit_lattice.solve(true);
 }

lattice_solver/Cargo.toml
@@ -26,9 +26,6 @@ harness = false
 [[example]]
 name = "solve"
 
-[[example]]
-name = "3d"
-
 [profile.bench]
 debug = true 
 

lattice_solver/examples/3d.rs
@@ -1,38 +1,0 @@
-use std::{fs::File, io::Read};
-use itertools::Itertools;
-use lattice_solver::Lattice;
-
-
-fn main() {
-    // let delta_y: f32 = 1.4;
-
-    // let mut buffer = String::new();
-    // let mut file = File::open("../DFT_results/T1S.json").unwrap();
-    // file.read_to_string(&mut buffer).unwrap();
-    // let parsed = json::parse(&buffer).unwrap();
-
-    // let last_id = &parsed["ids"][parsed["ids"].len() - 1].to_string();
-
-    // let hydrogen_amount = &parsed[last_id]["numbers"]["__ndarray__"][2]
-    //     .members()
-    //     .filter_map(|n| n.as_usize())
-    //     .filter(|n| n == &1)
-    //     .count();
-    // let numbers = &parsed[last_id]["positions"]["__ndarray__"][2];
-    // let atoms = numbers.members()
-    //     .map(|j| j.as_f32().unwrap())
-    //     .collect_vec()
-    //     .chunks_exact(3)
-    //     .map(|c| c.to_vec())
-    //     .collect_vec();
-
-    // let ends = atoms.iter()
-    //     .sorted_by(|&a, &b| a[2].total_cmp(&b[2]))
-    //     .take(hydrogen_amount.div_ceil(2))
-    //     .collect_vec();
-    let lattice = Lattice::from_dft_json("../DFT_results/T1S.json".into(), 1.3);
-
-    // for atom in ends {
-    //     println!("{atom:?}");
-    // }
-}

commit 0bcd09a59814c9dadffb6c7e90fd365c9d7fa6ee

Fixed critical bug introduced in b68d78f, which caused ghost point connections to be ignored.

lattice_solver/examples/solve.rs
@@ -68,6 +68,7 @@ fn main() {
 
     let options = BitArraySettings::create(2, 0.2, lattice.find_max());
     let bit_lattice = lattice.get_intermediary(options);
+
     // bit_lattice.print_distances();
     println!("{}", bit_lattice.__str__());
     println!("{}", bit_lattice.exclusion_matrix[0][0]);

lattice_solver/src/lib.rs
@@ -115,7 +115,7 @@ impl LatticePoint {
     fn get_connections(&self) -> &RwLock<Vec<OxygenIndex>> {
         self.ghost_to
             .as_ref()
-            .map_or(&self.connected_to, |_| &self.connected_to)
+            .map_or(&self.connected_to, |point| &point.connected_to)
     }
 
     #[allow(clippy::suboptimal_flops)]