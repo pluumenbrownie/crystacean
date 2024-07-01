1fa550bed58bbcca0c4dae9d76c4f429eb8ea5ec
Pluumenbrownie <git.wessel@beumertjes.nl>
2024-07-01 15:08:20 +0200

    Added points to bench

--- lattice_solver/benches/divan_bench.rs
+++ lattice_solver/benches/divan_bench.rs
@@ -1,20 +1,44 @@
+#![allow(clippy::excessive_precision)]
 use lattice_solver::{
     Lattice, BitArraySolution
 };
 
-// #[divan::bench(max_time = 120, args = [true, false])]
-// fn full_run_parallel(silent: bool) -> Vec<lattice_solver::BitArraySolution> {
-//     let lattice_points = lattice_points();
-
-//     let lattice = Lattice::python_new(lattice_points, 1.1, true);
-
-//     let options = bit_array_settings!(lattice);
-//     let bit_lattice = lattice.get_intermediary(options);
-
-//     let result = bit_lattice.solve_parallel(true, silent);
-//     print!("\r{}{}", cursor::Right(22), clear::AfterCursor);
-//     result
-// }
+fn lattice_points() -> Vec<(Vec<f32>, Vec<Vec<f32>>)> {
+    vec![
+        (vec![1.5, 0.0], vec![vec![1.5, 5.196152422706632]]),
+        (
+            vec![0.0, 0.0],
+            vec![
+                vec![0.0, 5.196152422706632],
+                vec![6.0, 0.0],
+                vec![6.0, 5.196152422706632],
+            ],
+        ),
+        (vec![2.25, 1.299038105676658], vec![]),
+        (
+            vec![0.75, 1.299038105676658],
+            vec![vec![6.75, 1.299038105676658]],
+        ),
+        (vec![1.5, 2.598076211353316], vec![]),
+        (
+            vec![0.0, 2.598076211353316],
+            vec![vec![6.0, 2.598076211353316]],
+        ),
+        (vec![2.25, 3.897114317029974], vec![]),
+        (
+            vec![0.75, 3.897114317029974],
+            vec![vec![6.75, 3.897114317029974]],
+        ),
+        (vec![4.5, 0.0], vec![vec![4.5, 5.196152422706632]]),
+        (vec![3.0, 0.0], vec![vec![3.0, 5.196152422706632]]),
+        (vec![5.25, 1.299038105676658], vec![]),
+        (vec![3.75, 1.299038105676658], vec![]),
+        (vec![4.5, 2.598076211353316], vec![]),
+        (vec![3.0, 2.598076211353316], vec![]),
+        (vec![5.25, 3.897114317029974], vec![]),
+        (vec![3.75, 3.897114317029974], vec![]),
+    ]
+}
 
 #[divan::bench(max_time = 120)]
 fn huge_run() -> Vec<BitArraySolution> {
